6857be6e9e631e3267f1ce2eb1cf8bfd5fd6c6ce
lildud <45979134+imlildud@users.noreply.github.com>
2025-02-18 23:21:04 -0600

    Update and rename 03-MetodosCombinacionClass.java to 03-MetodosFactorial.java

--- 03-MetodosFactorial.java
+++ 03-MetodosFactorial.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
-public class MetodosCombinacion{
+
+public class MetodosFactorial {
     public static void main(String[] args){
 
         Combinacion obs = new Combinacion();
